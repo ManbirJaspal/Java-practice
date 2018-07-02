9aa96adcaedd48e109e0597aa2c1157b2539da88
manbirsinghjaspal <manb.jaspal@gmail.com>
2018-07-02 13:48:05 -0400

    Collections

--- GenericsChallenge/src/com/Manbir/Main.java
+++ GenericsChallenge/src/com/Manbir/Main.java
@@ -5,6 +5,16 @@ public class Main {
     public static void main(String[] args) {
 	League<Team<Footballplayer>> footballLeague = new League<>("AFL");
 
-	
+
+        Team<Footballplayer> adelaideCrows = new Team<>("AdelaideCrows");
+        Team<Footballplayer> melbourne = new Team<>("Melbourne");
+        Team<Footballplayer> hawthorn = new Team<>("Hawthorn");
+        Team<Footballplayer> fremantle = new Team<>("Fremantle");
+
+        footballLeague.add(adelaideCrows);
+        footballLeague.add(melbourne);
+        footballLeague.add(hawthorn);
+        footballLeague.add(fremantle);
+
     }
 }
